15b28460e4f24b4bdd30c593204012b0ec607727
William Sullivan <sullivanwp95@gmail.com>
2022-07-09 00:17:01 -0400

    adding classes dropdown component

---
+++ src/components/Classes.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { Dropdown } from "react-bootstrap";
+
+function Classes() {
+    const [classData, setClassData] = useState([])
+
+    // retrieve possible character classes from API
+    useEffect(() => {
+        const API_URL = `http://localhost:3001/classes`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setClassData(resData.results)
+        }
+        fetchData()
+    }, [])
+
+    // iterate through classes to return each item individually
+    const classes = classData.map((singleClass, i) => {
+        return (
+            <Dropdown.Item key={i}>{singleClass.name}</Dropdown.Item>
+        )
+    })
+
+    return (
+        <div>
+            <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Classes
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    {classes}
+                </Dropdown.Menu>
+            </Dropdown>
+        </div>
+    )
+}
+
+export default Classes
--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -1,9 +1,11 @@
+import Classes from "./Classes"
 import Spells from "./Spells"
 
 function HomeView() {
     return (
         <div>
             <h1>Home Page</h1>
+            <Classes />
             <Spells />
         </div>
     )
