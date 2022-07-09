82b5112d8c6d800dface087032ab74448badfa82
William Sullivan <sullivanwp95@gmail.com>
2022-07-09 17:58:26 -0400

    adding profile route

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -15,9 +15,22 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password` 
         })
     } else {
+        req.session.user_id = user.user_id
         res.json({ user })
     }
 })
 
+router.get('/profile', async (req, res) => {
+    try {
+        let user = await User.findOne({
+            where: {
+                user_id: req.session.user_id
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
 
 module.exports = router
