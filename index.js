1603076df9fc8a81d64d36c1e0efdc2165b06c32
William Sullivan <sullivanwp95@gmail.com>
2022-07-09 11:55:36 -0400

    creating some test routes

--- index.js
+++ index.js
@@ -10,6 +10,45 @@ const { Sequelize } = require('sequelize')
 app.use(cors())
 
 // routes
+app.get('/spells', async(req, res) => {
+    try {
+        let response = await axios.get('https://www.dnd5eapi.co/api/spells')
+        res.status(200).send(response.data)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error)
+    }
+})
+
+app.get('/spells/:name', async(req, res) => {
+    try {
+        let response = await axios.get(`https://www.dnd5eapi.co/api/spells/${req.params.name}`)
+        res.status(200).send(response.data)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error)
+    }
+})
+
+app.get('/classes', async(req, res) => {
+    try {
+        let response = await axios.get('https://www.dnd5eapi.co/api/classes')
+        res.status(200).send(response.data)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error)
+    }
+})
+
+app.get('/races', async(req, res) => {
+    try {
+        let response = await axios.get('https://www.dnd5eapi.co/api/races')
+        res.status(200).send(response.data)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error)
+    }
+})
 
 // listener
 app.listen(process.env.PORT || 4000, () => console.log(`Listening on ${process.env.PORT || 4000}`))
