a01364f4cf5ebc380e6180b9e6be323f450e545c
William Sullivan <sullivanwp95@gmail.com>
2022-07-09 15:53:52 -0400

    creating login form

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NewCharacter from './components/NewCharacter';
 import SignUp from './components/SignUp';
+import Login from './components/Login';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<SignUp />} />
+        <Route path='/' element={<Login />} />
         <Route path='/new_character' element={<NewCharacter />} />
       </Routes>
     </Router>
---
+++ src/components/LogIn.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+// import { useNavigate } from "react-router";
+
+function Login() {
+
+    // const navigate = useNavigate()
+
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+    })
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        const response = await fetch(`http://localhost:3001/authentication`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
+
+        const data = await response.json()
+
+        console.log(data)
+
+    }
+    
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="row">
+                    <div className="col-sm-6 form-group">
+                        <label htmlFor="username">Username</label>
+                        <input
+                            required
+                            value={credentials.username}
+                            onChange={e => setCredentials({ ...credentials, username: e.target.value })}
+                            className="form-control"
+                            id="username"
+                            name="username"
+                        />
+                    </div>
+                    <div className="col-sm-6 form-group">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            required
+                            value={credentials.password}
+                            onChange={e => setCredentials({ ...credentials, password: e.target.value })}
+                            className="form-control"
+                            id="password"
+                            name="password"
+                        />
+                    </div>
+                </div>
+                <input className="btn btn-primary" type="submit" value="Login" />
+            </form>
+        </div>
+    )
+}
+
+export default Login
