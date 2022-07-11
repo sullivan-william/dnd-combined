7fa80cf8716b6edf139334dd28b9926df57c5c91
William Sullivan <sullivanwp95@gmail.com>
2022-07-10 22:07:09 -0400

    building new character post route

--- controllers/characters.js
+++ controllers/characters.js
@@ -1,16 +1,21 @@
 const router = require('express').Router()
 const db = require("../models")
 
-const { Character, User } = db
+const { Character } = db
 
 router.post('/new', async (req, res) => {
-    console.log(req.body)
-    // const character = await Character.create({
-    //     name: req.body.name,
-    //     race: req.body.race,
-    //     class: req.body.class
-    // })
-    // res.json(character)
+    try {
+        console.log(req.body)
+        const character = await Character.create({
+            name: req.body.name,
+            race: req.body.race,
+            class: req.body.class,
+            user_id: req.body.user_id
+        })
+        res.json(character)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 module.exports = router
